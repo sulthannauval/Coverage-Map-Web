04f20cb8d364b93de0b48616c13b1b7095490bf3
Hyumseok <salupaeee@gmail.com>
2025-05-02 17:32:55 +0700

    Update Admin and Login
    Login:
    Backbutton
    
    Admin:
    halaman baru (admineditupload.blade.php)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+        rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
@@ -12,11 +15,18 @@
 
 <body>
     <div class="login-form">
-        <h1>Login</h1>
+        <!-- <h1>Login</h1> -->
         <div class="container">
             <div class="mainlogin">
                 <div class="content">
-                    <h2>Log In</h2>
+                    <dix class="row">
+                        <div class="col-4">
+                            <a class="backbutton" href="/" style="font-size: 28px;">
+                                <i class="ri-arrow-left-line"></i>
+                            </a>
+                        </div>
+                        <div class="col-4"><h2>Log In</h2></div>
+                    </dix>
                     <form  method="POST" action="{{ route('login') }}">
                         @csrf
                         <Input type="text" name="login" id="login" placeholder="Email or Username" required autofocus="">
